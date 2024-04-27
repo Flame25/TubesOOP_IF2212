30dede785148a9f5314bafc488ec76d5e5b5c13f
Flame25 <noobguideforyou@gmail.com>
2024-04-28 02:00:31 +0700

    added working Dummy map

--- src/main/java/org/game/GamePanel.java
+++ src/main/java/org/game/GamePanel.java
@@ -3,19 +3,22 @@ package org.game;
 import javax.swing.*;
 import java.awt.*;
 import org.asset.*;
+import org.map.TileManager;
 
 public class GamePanel extends JPanel implements  Runnable {
     final int originalTileSize = 16; // 16 x 16 tile
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48 x 48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
 	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 	
 	// FPS 
-	int FPS = 60; 
+	int FPS = 60;
+
+	TileManager tileManager = new TileManager(this);
 	KeyHandler keyH = new KeyHandler(); 
 	Thread gameThread; // Game Clock
 	Player player = new Player(this, keyH);
@@ -73,6 +76,8 @@ public class GamePanel extends JPanel implements  Runnable {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+
+		tileManager.draw(g2);
 		player.draw(g2);
 		g2.dispose();
 		
